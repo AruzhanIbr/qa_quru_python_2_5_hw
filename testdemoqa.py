3669eaa09fa23123152467194276ad2d42075355
AruzhanIbr <ibragimova080499@gmail.com>
2022-09-10 03:44:02 +0600

    добавила def test_one

--- testdemoqa.py
+++ testdemoqa.py
@@ -1,5 +1,6 @@
+import os
+from selene import have
 from selene.support.shared import browser
-from selene import command, be
 
 
 def test_one():
@@ -14,4 +15,26 @@ def test_one():
     browser.element('[aria-label="Choose Monday, March 6th, 1995"]').click()
     browser.element('#subjectsInput').type('Maths').press_enter().type('History').press_enter()
     browser.element('[for="hobbies-checkbox-2"]').click()
-    browser.element('#uploadPicture').send_keys()
+    browser.element('#uploadPicture').send_keys(os.path.abspath('files/test.jpg'))
+    browser.element('#currentAddress').type('Astana')
+    browser.element('#react-select-3-input').type('Haryana').press_enter()
+    browser.element('#react-select-4-input').type('Karnal').press_enter()
+    browser.element('#submit').click()
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    browser.all('.modal-body td+td').should(have.texts(
+        'Elena Romanova',
+        'elena@mail.ru',
+        'Female',
+        '1234567891',
+        '06 March,1995',
+        'Maths, History',
+        'Reading',
+        'test.jpg',
+        'Astana',
+        'Haryana Karnal'))
+
+
+
+
+
+
