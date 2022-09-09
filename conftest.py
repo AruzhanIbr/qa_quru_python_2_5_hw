1a34946a0bf158548c36ab8a4bd9bc076eeb76ed
AruzhanIbr <ibragimova080499@gmail.com>
2022-09-10 03:43:26 +0600

    добавила yield

--- conftest.py
+++ conftest.py
@@ -1,10 +1,14 @@
-
+from selene import command, have
 from selene.support.shared import browser
 import pytest
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    browser.open('https://demoqa.com/automation-practice-form')
     browser.config.hold_browser_open = True
+    browser.open('https://demoqa.com/automation-practice-form')
+    ads = browser.all('[id^=google_ads_][id$=container__]')
+    ads.should(have.size_less_than_or_equal(3))
+    ads.perform(command.js.remove)
     yield
+    browser.element("#closeLargeModal").click()
